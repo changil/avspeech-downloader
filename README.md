bff18300dd2ca5ea2605978e37837dfb15f431d4
Changil Kim <changilkim@gmail.com>
2019-01-29 21:10:13 -0500

    transcode streams by default when cutting videos (resolves #1)

--- README.md
+++ README.md
@@ -5,25 +5,28 @@ A bash script to download [AVSpeech](https://looking-to-listen.github.io/avspeec
 ```bash
 $ download.sh path-to-csv-file output-directory
 ```
-You may stop the downloader by pressing `Ctrl-C`. Next runs recognize already downloaded files and do not try to re-download them.
+You may stop the downloader by pressing `Ctrl-C`. Next runs recognize already downloaded files and will not try to re-download them.
 
 ## Options
 `download.sh` acknowledges the following environment variables.
-* `dryrun` set to any non-zero value renders the downloader only print commands to the console
-* `njobs` sets the number of concurrent downloader processes to be run and defaults to the number of CPU cores
-* `faster` set to a non-zero value, the downloader tries to download 720p HD videos despite the existence of higher quality videos, which allows the downloader to run much faster since no transcoding is carried out
+* `dryrun` set to any nonzero value makes the downloader print commands to the console without downloading any videos.
+* `njobs` sets the number of concurrent downloader processes to run and defaults to the number of CPU cores.
+* With `faster` set to `1`, the downloader tries to download 720p or 360p videos instead of the highest quality videos. Additionally, when `faster` is set to `2`, the downloader forces _FFmpeg_ to copy streams, running much faster with no transcoding carried out. See the known issue below.
+
+## Required software packages
+The following packages are required to run the downloader. It is important to have the latest versions of both _youtube-dl_ and _FFmpeg_.
+* [youtube-dl](https://rg3.github.io/youtube-dl/)
+* [FFmpeg](https://www.ffmpeg.org/)
+* [GNU Parallel](https://www.gnu.org/software/parallel/)
 
 ## Examples
 ```bash
 $ download.sh avspeech_train.csv data/train
-$ njobs=1 faster=1 download.sh avspeech_train.csv data/train
+$ njobs=12 faster=1 download.sh avspeech_train.csv data/train
 ```
 
-## Required software packages
-The following packages are required to run the downloader.
-* [youtube-dl](https://rg3.github.io/youtube-dl/)
-* [FFmpeg](https://www.ffmpeg.org/)
-* [GNU Parallel](https://www.gnu.org/software/parallel/)
+## Known issue
+_FFmpeg_ uses keyframe seeking when stream copying, which happens with `faster=2`. When a cut does not start from a keyframe, which happens most of the time, it cuts the video at the closest preceding keyframe and sets a negative start time to compensate for it. Thus, any subsequent tools that take the cut video clips as input should take the start time into account. Most video players do, but if you programatically process video clips, chances are you need to do it yourself and discard the first part of both audio and video streams accordingly.
 
 ## Contact
 Please contact [Changil Kim](mailto:changil@csail.mit.edu) if you have questions of comments.
--- download.sh
+++ download.sh
@@ -2,25 +2,25 @@
 # download AVSpeech dataset
 #
 # usage:      download.sh <path-to-csv-file> <output-directory>
-# env vars:   dryrun={0|1} njobs=<n> faster={0|1}
-# dependency: youtube-dl, ffmpeg, parallel
+# env vars:   dryrun={0|1} njobs=<n> faster={0|1|2}
+# dependency: youtube-dl, FFmpeg, GNU Parallel
 # author:     Changil Kim <changil@csail.mit.edu>
 #
 # examples:
 #   download.sh avspeech_train.csv data/train
-#   njobs=1 faster=1 download.sh avspeech_train.csv data/train
+#   njobs=12 faster=1 download.sh avspeech_train.csv data/train
 
 # read environment variables
-# dry run if set (default: unset)
+# dry run if set nonzero (default: 0)
 dryrun="${dryrun:-0}"
 # number of parallel tasks (default: #cpu cores)
 njobs="${njobs:-0}"
-# faster download if set (always download 720p video; default: unset)
+# faster download if set > 0 (1: always download 720p/360p videos; 2: plus, use stream copy; default: 0)
 faster="${faster:-0}"
 
 # check if all arguments are given
 if [[ $# -ne 2 ]]; then
-	echo "usage: $(basename "$0") <csv-file> <out-dir>"
+	echo "usage: $(basename "$0") <path-to-csv-file> <output-directory>"
 	exit 1
 fi
 
@@ -82,34 +82,35 @@ download_video() {
 	duration="$(bc <<< "$end - $start")"
 	t="$(date +%s.%N)"
 
+	# get the source video (either downloaded file or url)
 	if [[ "$faster" -eq 0 ]]; then
-		# download the highest quality video
-
-		# download video
+		# download the highest quality video (which would require youtube-dl to mux video and audio that are downloaded separately)
 		$youtubedl --format 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio' --merge-output-format mp4 --prefer-ffmpeg --output "$outdir/$filename.inprogress.1" -- "$ytid" || { show_msg "$id" "ERROR: failed to download video"; return; }
 		# youtube-dl appends additional extension when merging video & audio - remove it
 		$mv "$outdir/$filename.inprogress.1.mp4" "$outdir/$filename.inprogress.1" || { show_msg "$id" "ERROR: failed to rename temporary video"; return; }
-
-		# cut video
-		$ffmpeg -ss "$start" -i "$outdir/$filename.inprogress.1" -t "$duration" -c:v copy -c:a copy -f mp4 -threads 1 "$outdir/$filename.inprogress.2" < /dev/null || { show_msg "$id" "ERROR: failed to cut video"; return; }
-
-		# cleanup
-		$rm "$outdir/$filename.inprogress.1" || { show_msg "$id" "ERROR: failed to remove temporary file"; return; }
-		$mv "$outdir/$filename.inprogress.2" "$outdir/$filename" || { show_msg "$id" "ERROR: failed to rename video"; return; }
-
+		src="$outdir/$filename.inprogress.1"
+		deletesrc=1
 	else
-		# download 720p hd video
-
-		# get video link (try 720p/360p mp4 format in that order)
-		url="$($youtubedl --get-url --format '22/18' -- "$ytid")" || { show_msg "$id" "ERROR: failed to get video url"; return; }
-
-		# download (only the interesting part of) video
-		$ffmpeg -ss "$start" -i "$url" -t "$duration" -c:v copy -c:a copy -f mp4 -threads 1 "$outdir/$filename.inprogress" < /dev/null || { show_msg "$id" "ERROR: failed to download video"; return; }
-
-		# cleanup
-		$mv "$outdir/$filename.inprogress" "$outdir/$filename" || { show_msg "$id" "ERROR: failed to rename video"; return; }
-
+		# get the url of the 720p/360p mp4 video (tried in that order; usually the best single file format)
+		src="$($youtubedl --get-url --format '22/18' -- "$ytid")" || { show_msg "$id" "ERROR: failed to get video url"; return; }
+		deletesrc=0
 	fi
+
+	# download/cut only the relevant part of video
+	if [[ "$faster" -le 1 ]]; then
+		# transcode streams (the following options should be reasonable for most cases, but could be changed to meet your taste)
+		codec_opts="-c:v libx264 -crf 18 -preset veryfast -pix_fmt yuv420p -c:a aac -b:a 128k -strict experimental"
+	else
+		# copy streams - this will result in the cut taking place at the closest preceding keyframe and the start time set accordingly (i.e., negative)
+		codec_opts="-c:v copy -c:a copy"
+	fi
+	$ffmpeg -ss "$start" -i "$src" -t "$duration" $codec_opts -f mp4 -threads 1 "$outdir/$filename.inprogress" < /dev/null || { show_msg "$id" "ERROR: failed to download/cut video"; return; }
+
+	# cleanup
+	if [[ $deletesrc -ne 0 ]]; then
+		$rm "$src" || { show_msg "$id" "ERROR: failed to remove temporary file"; return; }
+	fi
+	$mv "$outdir/$filename.inprogress" "$outdir/$filename" || { show_msg "$id" "ERROR: failed to rename video"; return; }
 
 	walltime="$(printf "%.1f" "$(bc <<< "$(date +%s.%N) - $t")")"
 	show_msg "$id" "downloaded (${walltime}s)"
